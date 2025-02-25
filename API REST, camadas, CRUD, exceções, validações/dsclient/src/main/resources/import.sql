45543a9440a2a8e02cb88d453b45ffea0f71932e
Matheus Farias <matheusaf.contato@gmail.com>
2025-02-25 05:58:26 -0300

    Seeding DataBase

---
+++ API REST, camadas, CRUD, exceções, validações/dsclient/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Matheus Aguiar Farias', '952.642.120-17', 2700.0, '1998-08-19', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alessandra Bombardi Rocha', '922.145.752-29', 800.0, '1999-10-24', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Michele Aparecida da Silva', '152.622.875-19', 2000.0, '1988-04-30', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rochele da Silva Farias', '283.927.104-45', 3700.0, '1992-01-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Claudio Emir Farias Rosa', '923.192.012-93', 2650.0, '1968-04-30', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Beatriz Souza Silva', '234.567.890-12', 3200.0, '1985-11-15', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcos Antônio Pereira', '543.210.987-65', 4500.0, '1978-07-20', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana da Costa Almeida', '876.543.210-98', 3900.0, '1990-02-10', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Roberto Carlos Lima', '123.456.789-00', 5100.0, '1982-09-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Oliveira Santos', '112.233.445-66', 3800.0, '1993-06-25', 2);
+
+
