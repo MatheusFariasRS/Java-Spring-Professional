d71395f8072736ef0d1b695f69d9f33db3009033
Matheus Farias <matheusaf.contato@gmail.com>
2025-02-26 05:59:12 -0300

    GET Client by id

---
+++ API REST, camadas, CRUD, exceções, validações/dsclient/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsclient.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Client client = repository.findById(id).get();
+		return new ClientDTO(client);
+	}
+}
