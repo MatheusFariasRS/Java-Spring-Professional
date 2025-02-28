5a48b7b64ef1101f900c01be64d785d06d3b89a7
Matheus Farias <matheusaf.contato@gmail.com>
2025-02-28 04:49:45 -0300

    DELETE client

--- API REST, camadas, CRUD, exceções, validações/dsclient/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ API REST, camadas, CRUD, exceções, validações/dsclient/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -43,6 +43,11 @@ public class ClientService {
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
+	
+	@Transactional
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
