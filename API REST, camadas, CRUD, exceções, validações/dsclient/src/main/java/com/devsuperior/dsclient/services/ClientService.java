fb8671c6dd59123b11e735d18dc36380a5ec1e97
Matheus Farias <matheusaf.contato@gmail.com>
2025-02-28 04:37:42 -0300

    PUT client

--- API REST, camadas, CRUD, exceções, validações/dsclient/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ API REST, camadas, CRUD, exceções, validações/dsclient/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -15,31 +15,41 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(Pageable pageable) {
 		Page<Client> result = repository.findAll(pageable);
 		return result.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		Client entity = repository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
-		
-		entity = repository.save(entity);
-		return new ClientDTO(entity);
-		
-		
+
 	}
 }
